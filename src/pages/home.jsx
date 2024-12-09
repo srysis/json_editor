b15a2aeabc0038f12c550e4b56172b8e6ac0bb82
srysis <jashandenis@gmail.com>
2024-12-09 16:57:06 +0100

    changed 'intro_text' content: now all of the information about the app is inside the README file

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,24 +7,7 @@ function Home() {
 	return(
 		<>
 			<div id="intro_text">
-				<p>
-					This is a very simple JSON editor.
-					You can either create an empty file or you can import an existing JSON file, afterwhich you can choose what to do with it's content.
-					You can add new records to or remove old ones from the file. After your work has been done, you can save the file to your PC.
-				</p>
-				<p>
-					Please keep in mind that it is a rather limited service, as it only processes files of the following structure:
-				</p>
-				<pre>
-					[
-						&#123;
-							<em> id</em>: numeric,
-							<em> name</em>: string,
-							<em> surname</em>: string,
-							<em> bio</em>: string
-						&#125;
-					]
-				</pre>
+				<h2>Choose your option</h2>
 			</div>
 			<div className="flex_container navigation">
 				<div id="upload" onClick={() => { navigate("/upload") } }>
